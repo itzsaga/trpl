848300cc7ba4fd3c89bb75971f1e55c9dde62852
Seth <itz.saga@gmail.com>
2017-11-30 23:42:04 -0600

    Variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    // immutable by default
+    let y = 4;
+    println!("The value of y is {}", y);
+    // this will throw an error at compile time
+    // y = 5
+
+    // mutable
+    let mut x =5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
+
+    shadowing();
+}
+
+// an example of shadowing
+fn shadowing() {
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is {}", x);
 }
